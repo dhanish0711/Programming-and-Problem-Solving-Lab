46046524948afc19b9ab0caff4dca6c5038ef0da
dhanish0711 <dhanishladwani@gmail.com>
2025-05-05 22:55:26 +0530

    Create Experiment 09.py

---
+++ Experiment 09.py
@@ -0,0 +1,7 @@
+# Write a Python program to check if a given year is a leap year or not.
+
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 :
+	print("%d is a leap year" %(year))
+else:
+	print("%d is not a leap year" %(year))
