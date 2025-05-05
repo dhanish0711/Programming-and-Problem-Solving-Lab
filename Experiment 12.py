92e14c8e409edc3645c790b799f886c036f25dcb
dhanish0711 <dhanishladwani@gmail.com>
2025-05-05 23:00:19 +0530

    Create Experiment 12.py

---
+++ Experiment 12.py
@@ -0,0 +1,13 @@
+# Write a python program to print factorial of given number.
+
+def factorial(n):
+	if n == 0 or n == 1:
+		return 1
+	else:
+		return n * factorial(n - 1)
+
+num = int(input("Enter a number : "))
+if num < 0:
+	print("Enter a positive number")
+else:
+	print("Factorial of given number is :", factorial(num))
